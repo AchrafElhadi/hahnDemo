73f4179cf5b2ac9844a1f864294fbb28e95a075d
Achraf El Hadi <aelhadi@novelis.io>
2025-06-28 03:16:55 +0100

    add repository

---
+++ backend/src/main/java/com/hahn/demo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hahn.demo.repository;
+
+import com.hahn.demo.domaine.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
