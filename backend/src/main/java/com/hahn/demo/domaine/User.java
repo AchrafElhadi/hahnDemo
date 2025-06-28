e22b770dbb85d71c2e188fa03a038b14b34c26d6
Achraf El Hadi <aelhadi@novelis.io>
2025-06-28 03:59:23 +0100

    add getter and setter to user entity

--- backend/src/main/java/com/hahn/demo/domaine/User.java
+++ backend/src/main/java/com/hahn/demo/domaine/User.java
@@ -1,9 +1,13 @@
 package com.hahn.demo.domaine;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
