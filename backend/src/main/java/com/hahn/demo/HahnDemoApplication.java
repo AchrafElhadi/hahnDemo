5c1ac1b158f2b9f69532cc98cb463624f9b456c1
Achraf El Hadi <aelhadi@novelis.io>
2025-06-28 03:08:36 +0100

    create user using liquibase & add entity

---
+++ backend/src/main/java/com/hahn/demo/HahnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hahn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HahnDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/hahn/demo/domaine/User.java
@@ -0,0 +1,24 @@
+package com.hahn.demo.domaine;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String pays;
+}
