1ae33883ad369be8b4a0dccb89aab43184485ad6
Achraf El Hadi <aelhadi@novelis.io>
2025-06-29 18:42:11 +0100

    initiate project and add config

---
+++ frontend/src/config/apiConfig.js
@@ -0,0 +1,13 @@
+export const API_CONFIG = {
+  BASE_URL: 'http://localhost:8080/api',
+  ENDPOINTS: {
+    USERS: '/users',
+  },
+  DEFAULT_HEADERS: {
+    'Content-Type': 'application/json',
+  },
+  PAGINATION: {
+    DEFAULT_PAGE: 0,
+    DEFAULT_SIZE: 10,
+  },
+};
